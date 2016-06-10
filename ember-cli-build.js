9477fea5426edc556e75aabb6631abc5fde4add9
Igor Rogatty <igor@wikia-inc.com>
2016-06-10 17:23:51 +0200

    inline css

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,6 +5,7 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function (defaults) {
 	var app = new EmberApp(defaults, {
 		inlineContent: {
+			css: 'vendor/app.css',
 			svg: 'vendor/svg.html'
 		}
 	});
